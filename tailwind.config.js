924303389e885fd809a2acdc85483fc09f155720
Lars Skelboe <lars@skelboe.dk>
2020-12-27 11:57:51 +0100

    Remove attributes from tailwindcss 1.x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,4 @@
 module.exports = {
-    future: {
-        // https://tailwindcss.com/docs/upcoming-changes#remove-deprecated-gap-utilities
-        removeDeprecatedGapUtilities: true,
-        // https://tailwindcss.com/docs/upcoming-changes#purge-layers-by-default
-        purgeLayersByDefault: true,
-        // https://tailwindcss.com/docs/upcoming-changes#default-line-heights-for-font-size-utilities
-        defaultLineHeights: true,
-        // https://tailwindcss.com/docs/upcoming-changes#rename-font-thin-and-font-hairline
-        standardFontWeights: true,
-    },
     purge: {
         enabled: process.env.NODE_ENV == 'production',
         content: ['./pages/**/*.{jsx,tsx}', './components/**/*.{jsx,tsx}'],

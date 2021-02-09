42fe49a1bef5b7102229bb5a60c59f3cc16d6c0e
Lars Skelboe <lars@skelboe.dk>
2021-02-09 13:45:41 +0100

    Add lib to folders for tailwind to look at

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     purge: {
         enabled: process.env.NODE_ENV == 'production',
-        content: ['./pages/**/*.{jsx,tsx}', './components/**/*.{jsx,tsx}'],
+        content: ['./pages/**/*.{jsx,tsx}', './components/**/*.{jsx,tsx}', './lib/**/*.{jsx,tsx}'],
         whitelist: ['body', 'html'],
     },
     theme: {
